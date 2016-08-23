39739d9c7d6d9e7e726ebdae2e234559ef89b147
atsatsou <atsatsou92@gmail.com>
2016-08-23 18:38:58 +0300

    add the new main

--- src/main/java/com/mahi/model/Main.java
+++ src/main/java/com/mahi/model/Main.java
@@ -6,14 +6,14 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		List<Customers> customerList =new ArrayList<>();
+
+		List<Customers> customerList = new ArrayList<Customers>();
 		Customers c=new Customers("mahi","+306574815455","Leoforos Stratou 62");
 		customerList.add(c);
 		System.out.println("Informations about my customers list:"+" "+c);
-		
-	
-		
+
+	 System.out.println("mahi");
+
 	}
 
 }
